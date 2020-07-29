ece42da575724fd011cae983ab15d43ae2d4e507
i.zhilin <izhilin@studiofact.ru>
2020-07-29 23:05:00 +0500

    Initial Commit

---
+++ BiletixTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiletixTest
+//
+//  Created by Иван Жилин on 29.07.2020.
+//  Copyright © 2020 Иван Жилин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
